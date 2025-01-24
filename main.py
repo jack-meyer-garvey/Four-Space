9e9e1ca0332a14ab825d3c9a6d5bd226427a11ce
jackm <jack.meyergarvey@gmail.com>
2025-01-23 22:42:01 -0600

    Bonk Bop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from collections import deque
-import textwrap
 
 
 def createWindow(x: int, y: int):
@@ -11,6 +9,7 @@ def createWindow(x: int, y: int):
     c = Canvas(r, width=x, height=y, bg='black')
     c.focus_set()
     c.pack()
+    c.focus_set()
     return r, c
 
 
@@ -48,11 +47,6 @@ class overlay:
         self.drawGridLeft(nX, nY)
         self.drawRightBox()
         self.drawTextbox()
-        # memory associated with resolving textbox Text
-        self.textLines = deque()
-        self.typingLoop = None
-        self.lenLineShown = 0
-        self.typingSpeed = 31
 
     def drawGridLeft(self, nX, nY):
         if self.leftGridIndex is not None:
@@ -68,57 +62,17 @@ class overlay:
                                                       outline='white', width=4 * resize)
 
     def drawTextbox(self):
-        # noinspection PyTypeChecker
         self.textboxIndex = canvas.create_rectangle(34 * resize, 272 * resize, 566 * resize, 388 * resize,
                                                     outline='white', width=4 * resize, state=HIDDEN)
         self.textboxText = canvas.create_text(46 * resize, 278 * resize, text='', fill="white", anchor=NW,
                                               font=('System', int(12 * resize)), width=508 * resize)
 
-    def runTextboxFile(self, fileName: str):
-        canvas.itemconfigure(self.textboxIndex, state=NORMAL)
-        for line in open(fileName).readlines():
-            if line[0] == '#':
-                self.textLines.append(textwrap.fill(line[1:], 50))
-            elif line[:8] == 'command:':
-                print(line[8:])
-            else:
-                self.textLines.append(textwrap.fill(line, 50))
-        self.nextLine()
-
-    def nextLine(self):
-        if not self.typingLoop:
-            if self.textLines:
-                self.typeLetter()
-            else:
-                canvas.itemconfigure(self.textboxIndex, state=HIDDEN)
-                canvas.itemconfigure(self.textboxText, text="")
-                self.lenLineShown = 0
-
-    def typeLetter(self):
-        if self.lenLineShown != len(self.textLines[0]):
-            self.typingLoop = root.after(self.typingSpeed, self.typeLetter)
-            self.lenLineShown += 1
-            canvas.itemconfigure(self.textboxText, text=self.textLines[0][:self.lenLineShown])
-        else:
-            self.typingLoop = None
-            self.lenLineShown = 0
-            self.textLines.popleft()
-
-    def skipTyping(self):
-        if self.typingLoop:
-            self.lenLineShown = len(self.textLines[0]) - 1
-
-
-resize = 1
+resize = 2
 root, canvas = createWindow(int(600 * resize), int(400 * resize))
 
 main = overlay()
 main.drawGridLeft(6, 6)
-x, y = main.gridToScreen(3, 5)
+x, y = main.gridToScreen(1, 0)
 b = canvas.create_rectangle(x, y, x + main.sideX, y + main.sideY, fill='medium sea green', width=2 * resize,
                             outline='white')
-canvas.bind('<Button-1>', lambda event: main.nextLine())
-canvas.bind('<Button-3>', lambda event: main.skipTyping())
-canvas.focus_set()
-
 mainloop()
