c4f9f4b4cf43e7b92ad8a9959503000467afe709
jackm <jack.meyergarvey@gmail.com>
2025-01-26 12:10:31 -0600

    Bonk Bop

--- main.py
+++ main.py
@@ -25,20 +25,38 @@ def yCoordinateToScreen(y: float):
 def rotateXY(x, y, z, w, angle):
     return x * np.cos(angle) - y * np.sin(angle), x * np.sin(angle) + y * np.cos(angle), z, w
 
+def rotationMatrixXY(angle):
+    return np.matrix([[np.cos(angle), -np.sin(angle), 0, 0], [np.sin(angle), np.cos(angle), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
 def rotateXZ(x, y, z, w, angle):
     return x * np.cos(angle) - z * np.sin(angle), y, x * np.sin(angle) + z * np.cos(angle), w
+
+def rotationMatrixXZ(angle):
+    return np.matrix([[np.cos(angle), 0, -np.sin(angle), 0], [0, 1, 0, 0], [np.sin(angle), 0, np.cos(angle), 0], [0, 0, 0, 1]])
 
 def rotateXW(x, y, z, w, angle):
     return x * np.cos(angle) - w * np.sin(angle), y, z, x*np.sin(angle) + w*np.cos(angle)
 
+def rotationMatrixXW(angle):
+    return np.matrix([[np.cos(angle), 0, 0, -np.sin(angle)], [0, 1, 0, 0], [0, 0, 1, 0], [np.sin(angle), 0, 0, np.cos(angle)]])
+
 def rotateYZ(x, y, z, w, angle):
     return x, y * np.cos(angle) - z * np.sin(angle), y * np.sin(angle) + z * np.cos(angle), w
+
+def rotationMatrixYZ(angle):
+    return np.matrix([[1, 0, 0, 0], [0, np.cos(angle), -np.sin(angle), 0], [0, np.sin(angle), np.cos(angle), 0], [0, 0, 0, 1]])
 
 def rotateYW(x, y, z, w, angle):
     return x, y * np.cos(angle) - w * np.sin(angle), z, y*np.sin(angle) + w*np.cos(angle)
 
+def rotationMatrixYW(angle):
+    return np.matrix([[1, 0, 0, 0], [0, np.cos(angle), 0, -np.sin(angle)], [0, 0, 1, 0], [np.sin(angle), 0, 0, np.cos(angle)]])
+
 def rotateZW(x, y, z, w, angle):
     return x, y, z * np.cos(angle) - w * np.sin(angle), z * np.sin(angle) + w * np.cos(angle)
+
+def rotationMatrixZW(angle):
+    return np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, np.cos(angle), -np.sin(angle)], [0, 0, np.sin(angle), np.cos(angle)]])
 
 
 class overlay:
@@ -69,15 +87,62 @@ class overlay:
         self.textboxText = canvas.create_text(46 * resize, 278 * resize, text='', fill="white", anchor=NW,
                                               font=('System', int(12 * resize)), width=508 * resize)
 
+class fourSpace:
+    def __init__(self, xN, yN, zN, wN, sideLength):
+        self.xN = xN * sideLength
+        self.yN = yN * sideLength
+        self.zN = zN * sideLength
+        self.wN = wN * sideLength
+        self.gridLines = []
+        self.gridLinesIndex = []
+
+    def generateOutline(self):
+        faces = [[[-self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, -self.wN/2]],
+                [[-self.xN/2, self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, -self.wN/2]],
+                [[self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, -self.wN/2]],
+                [[self.xN/2, self.yN/2, -self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, -self.wN/2]],
+                [[self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, -self.wN/2]],
+                [[self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, self.wN/2]],
+                [[self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, -self.wN/2]],
+                [[self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, -self.wN/2]],
+                [[-self.xN/2, self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, self.wN/2]],
+                [[-self.xN/2, self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, -self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, self.yN/2, -self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, -self.zN/2, self.wN/2]],
+                [[-self.xN/2, -self.yN/2, self.zN/2, -self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, self.yN/2, self.zN/2, -self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, -self.wN/2]],
+                [[-self.xN/2, -self.yN/2, self.zN/2, self.wN/2], [-self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, self.yN/2, self.zN/2, self.wN/2], [self.xN/2, -self.yN/2, self.zN/2, self.wN/2]]]
+        for i, face in enumerate(faces):
+            self.gridLines.append(face)
+
+    def drawGridLines(self):
+        for line in self.gridLines:
+            self.gridLinesIndex.append(canvas.create_line(
+                xCoordinateToScreen(line[0][0]), yCoordinateToScreen(line[0][1]), xCoordinateToScreen(line[1][0]), yCoordinateToScreen(line[1][1]),
+                xCoordinateToScreen(line[1][0]), yCoordinateToScreen(line[1][1]), xCoordinateToScreen(line[2][0]), yCoordinateToScreen(line[2][1]),
+                xCoordinateToScreen(line[2][0]), yCoordinateToScreen(line[2][1]), xCoordinateToScreen(line[3][0]), yCoordinateToScreen(line[3][1]),
+                xCoordinateToScreen(line[3][0]), yCoordinateToScreen(line[3][1]), xCoordinateToScreen(line[0][0]), yCoordinateToScreen(line[0][1]),
+                fill='white', width=2, joinstyle='miter'))
+
+
 resize = 2
 root, canvas = createWindow(int(600 * resize), int(400 * resize))
 main = overlay()
 
-square = [-20, -20, -20, 20, 20, 20, 20, -20, -20, -20]
-for i in range(len(square)//2):
-    a, b = rotateZW(square[i*2], square[i*2+1], 0, 0, np.pi*3/4)[0:2]
-    square[i*2] = xCoordinateToScreen(a)
-    square[i*2+1] = yCoordinateToScreen(b)
-thing = canvas.create_line(*square, fill='white', width=4*resize, joinstyle='miter')
+tesseracts = fourSpace(5, 6, 3, 2, 10)
+tesseracts.generateOutline()
+tesseracts.drawGridLines()
+print(tesseracts.gridLinesIndex)
+
+
 
 mainloop()
