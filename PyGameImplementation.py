faf4b7564e420437297a5cd19ea249ec011a7fe0
jackm <jack.meyergarvey@gmail.com>
2025-03-30 18:46:14 -0500

    Bonk Bop

--- PyGameImplementation.py
+++ PyGameImplementation.py
@@ -1,51 +1,13 @@
 import pygame
 import numpy as np
 
-def tesseractPoints(x=1, y=1, z=1, w=1):
-    return list(np.array([[[-x, -y, -z, -w, 1], [-x, -y, -z, w, 1], [-x, -y, z, w, 1], [-x, -y, z, -w, 1]],
-                [[-x, y, -z, -w, 1], [-x, y, -z, w, 1], [-x, y, z, w, 1], [-x, y, z, -w, 1]],
-                [[x, -y, -z, -w, 1], [x, -y, -z, w, 1], [x, -y, z, w, 1], [x, -y, z, -w, 1]],
-                [[x, y, -z, -w, 1], [x, y, -z, w, 1], [x, y, z, w, 1], [x, y, z, -w, 1]],
-                [[-x, -y, -z, -w, 1], [-x, -y, -z, w, 1], [-x, y, -z, w, 1], [-x, y, -z, -w, 1]],
-                [[-x, -y, z, -w, 1], [-x, -y, z, w, 1], [-x, y, z, w, 1], [-x, y, z, -w, 1]],
-                [[x, -y, -z, -w, 1], [x, -y, -z, w, 1], [x, y, -z, w, 1], [x, y, -z, -w, 1]],
-                [[x, -y, z, -w, 1], [x, -y, z, w, 1], [x, y, z, w, 1], [x, y, z, -w, 1]],
-                [[-x, -y, -z, -w, 1], [-x, -y, z, -w, 1], [-x, y, z, -w, 1], [-x, y, -z, -w, 1]],
-                [[-x, -y, -z, w, 1], [-x, -y, z, w, 1], [-x, y, z, w, 1], [-x, y, -z, w, 1]],
-                [[x, -y, -z, -w, 1], [x, -y, z, -w, 1], [x, y, z, -w, 1], [x, y, -z, -w, 1]],
-                [[x, -y, -z, w, 1], [x, -y, z, w, 1], [x, y, z, w, 1], [x, y, -z, w, 1]],
-                [[-x, -y, -z, -w, 1], [-x, -y, -z, w, 1], [x, -y, -z, w, 1], [x, -y, -z, -w, 1]],
-                [[-x, -y, z, -w, 1], [-x, -y, z, w, 1], [x, -y, z, w, 1], [x, -y, z, -w, 1]],
-                [[-x, y, -z, -w, 1], [-x, y, -z, w, 1], [x, y, -z, w, 1], [x, y, -z, -w, 1]],
-                [[-x, y, z, -w, 1], [-x, y, z, w, 1], [x, y, z, w, 1], [x, y, z, -w, 1]],
-                [[-x, -y, -z, -w, 1], [-x, -y, z, -w, 1], [x, -y, z, -w, 1], [x, -y, -z, -w, 1]],
-                [[-x, -y, -z, w, 1], [-x, -y, z, w, 1], [x, -y, z, w, 1], [x, -y, -z, w, 1]],
-                [[-x, y, -z, -w, 1], [-x, y, z, -w, 1], [x, y, z, -w, 1], [x, y, -z, -w, 1]],
-                [[-x, y, -z, w, 1], [-x, y, z, w, 1], [x, y, z, w, 1], [x, y, -z, w, 1]],
-                [[-x, -y, -z, -w, 1], [-x, y, -z, -w, 1], [x, y, -z, -w, 1], [x, -y, -z, -w, 1]],
-                [[-x, -y, -z, w, 1], [-x, y, -z, w, 1], [x, y, -z, w, 1], [x, -y, -z, w, 1]],
-                [[-x, -y, z, -w, 1], [-x, y, z, -w, 1], [x, y, z, -w, 1], [x, -y, z, -w, 1]],
-                [[-x, -y, z, w, 1], [-x, y, z, w, 1], [x, y, z, w, 1], [x, -y, z, w, 1]]]))
-
-class fourSpace:
-    def __init__(self, xN, yN, zN, wN, sideLength):
-        self.xN = xN
-        self.yN = yN
-        self.zN = zN
-        self.wN = wN
-        self.sideLength = sideLength
-
-    def drawGrid(self):
-        pass
-
-
 resize = 1
+size = (int(600 * resize), int(400 * resize))
 pygame.init()
 pygame.display.set_caption("Four Space")
-screen = pygame.display.set_mode((int(600 * resize), int(400 * resize)))
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
-screen.fill("black")
 pygame.display.set_icon(pygame.image.load('FourSpaceLogo.png'))
 
 while running:
