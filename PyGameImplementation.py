c5ab9033748d1b554b7b1733a9dd782fea5422e9
jackm <jack.meyergarvey@gmail.com>
2025-03-30 20:54:35 -0500

    Bonk Bop

--- PyGameImplementation.py
+++ PyGameImplementation.py
@@ -10,10 +10,17 @@ clock = pygame.time.Clock()
 running = True
 pygame.display.set_icon(pygame.image.load('FourSpaceLogo.png'))
 
+hoi = np.zeros(size)
+hoi2 = np.zeros(size)+16711680
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            hoi, hoi2 = hoi2, hoi
+
+    pygame.surfarray.blit_array(screen, hoi)
     pygame.display.flip()
     clock.tick(60)  # limits FPS to 60
 pygame.quit()
